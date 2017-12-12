f70da240dee37888c88b0c3662878bb06944f4ac
Jon Haddad <jon@jonhaddad.com>
2017-12-11 21:28:18 -0500

    added client, seems to work ok

--- src/main/kotlin/com/rustyrazorblade/awfuldb/client/Client.kt
+++ src/main/kotlin/com/rustyrazorblade/awfuldb/client/Client.kt
@@ -3,22 +3,30 @@ package com.rustyrazorblade.awfuldb.client
 import com.google.protobuf.ByteString
 import com.rustyrazorblade.awfuldb.AwfulServer
 import com.rustyrazorblade.awfuldb.AwfulServiceGrpc
+import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
 import java.nio.charset.Charset
 
-
-class Client {
-    fun connect(address: String, port: Int) {
-        var channel = ManagedChannelBuilder.forAddress(address, port).usePlaintext(true).build()
-        var stub = AwfulServiceGrpc.newBlockingStub(channel)
-    }
+/*
+wrapper around the grpc sillyness
+currently takes strings but maybe it would be nicer to take native objects
+ */
+class Client constructor(address: String, port: Int) {
+    var channel: ManagedChannel = ManagedChannelBuilder.forAddress(address, port).usePlaintext(true).build()
+    var stub: AwfulServiceGrpc.AwfulServiceBlockingStub = AwfulServiceGrpc.newBlockingStub(channel)
 
     fun put(key: String, value: String) {
-
+        val put = AwfulServer.PutRequest.newBuilder().setKey(key).setValue(value).build()
     }
 
-    fun get(key: String) {
-
+    /*
+    returns the string response
+    should this be a json object?
+    not sure
+     */
+    fun get(key: String) : String {
+        val get = AwfulServer.GetRequest.newBuilder().setKey(key).build()
+        val response2 = stub.getValue(get)
     }
 
     fun select(key: String) {
@@ -26,7 +34,7 @@ class Client {
     }
 
     fun delete(key: String) {
-        
+
     }
 
 
@@ -35,28 +43,21 @@ class Client {
 fun main(args: Array<String>) {
     println("Running the test client.")
 
-    var client = Client()
-    client.connect("127.0.0.1", 5000)
-
-    var channel = ManagedChannelBuilder.forAddress("localhost", 5000).usePlaintext(true).build()
-
-    var stub = AwfulServiceGrpc.newBlockingStub(channel)
+    var client = Client("127.0.0.1", 5000)
 
     val key = "test"
     val value = "{\"key\":\"value\"}"
-
-    val put = AwfulServer.PutRequest.newBuilder().setKey(key).setValue(value).build()
-
     println("Putting $key, $value")
-    val response = stub.putValue(put)
+
+    val response = client.put(key, value)
 
     println("Done putting, $response")
 
-    val get = AwfulServer.GetRequest.newBuilder().setKey(key).build()
-    val response2 = stub.getValue(get)
+    val response2 = client.get(key)
 
     println("Get($key): $response2")
-    if(response2.value == value) {
+
+    if(response2 == value) {
         println("Got expected value")
     } else {
         println("Something fishy...")
